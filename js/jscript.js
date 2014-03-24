d195c6c4c880ff39e58ef6e897093e7143182b40
martinappo <martinappo@gmail.com>
2014-03-24 16:58:01 +0200

    esileht valmis
    Sain esilehe põhimõtteliselt valmis

---
+++ js/jscript.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+	 /** READ MORE CLICK **/
+	$('.readmore').click(function() {
+		var currentId = $(this).attr('id');
+		$('article.single-news#' + currentId).toggleClass("open", 200, "swing");
+	});
+
+	/****************************
+	*****************************
+
+	Logo slider
+
+	*****************************
+	****************************/
+
+	$('.logoscontainer').carouFredSel({
+		auto: true,
+		responsive: true,
+		width: '100%',
+		swipe: {
+			onMouse: true,
+			onTouch: true
+		},//swipe
+		items: {
+			width: 200,
+			visible:{
+				min:2,
+				max: 6
+			}
+		},//items
+		scroll:{
+			items: 1,
+			pauseOnHover: true
+		}//scroll
+	});//slider
+
+
+	/****************************
+	*****************************
+
+	Special slider
+
+	*****************************
+	****************************/
+
+	$('.specialcontainer').carouFredSel({
+		auto: true,
+		responsive: true,
+		width: '100%',
+		swipe: {
+			onMouse: true,
+			onTouch: true
+		},//swipe
+		items: {
+			width: 200,
+			visible:{
+				min:2,
+				max: 4
+			}
+		},//items
+		scroll:{
+			items: 1,
+			pauseOnHover: false
+		}//scroll
+	});//slider
+
+	/****************************
+	*****************************
+
+	Special slider
+
+	*****************************
+	****************************/
+
+	$('.slidercontainer').carouFredSel({
+		auto: true,
+		responsive: true,
+		width: '100%',
+		swipe: {
+			onMouse: true,
+			onTouch: true
+		},//swipe
+		items: {
+			width: 200,
+			visible:{
+				min:1,
+				max: 1
+			}
+		},//items
+		scroll:{
+			items: 1,
+			pauseOnHover: false
+		}//scroll
+	});//slider
+
+
+});
